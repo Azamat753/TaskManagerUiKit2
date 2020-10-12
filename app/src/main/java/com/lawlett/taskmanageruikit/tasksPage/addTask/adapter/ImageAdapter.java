4b2e8124dd0b51785f90b8578273a5fb49ab8c12
RavenRav <akilaia@inbox.ru>
2020-10-12 11:06:48 +0300

    Добавила Dialog

---
+++ app/src/main/java/com/lawlett/taskmanageruikit/tasksPage/addTask/adapter/ImageAdapter.java
@@ -0,0 +1,41 @@
+package com.lawlett.taskmanageruikit.tasksPage.addTask.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.lawlett.taskmanageruikit.R;
+
+public class ImageAdapter extends ArrayAdapter<Integer> {
+    private Integer[] images;
+
+    public ImageAdapter(Context context, Integer[] images) {
+        super(context, R.layout.item_dialogimage, images);
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initView(position);
+    }
+
+    private View initView(int position) {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(images[position]);
+        imageView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        return imageView;
+    }
+}
